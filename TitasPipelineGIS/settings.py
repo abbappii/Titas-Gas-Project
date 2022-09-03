d05aa29addeb53fa649bf596dd27f502ffb2bc0c
abbappii <bappi142434@gmail.com>
2022-09-03 07:58:30 +0600

    settings updated

--- TitasPipelineGIS/settings.py
+++ TitasPipelineGIS/settings.py
@@ -242,6 +242,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'AuthenticationApp.User'
 
 # Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env('EMAIL_PORT')
