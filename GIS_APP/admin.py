bd7676675d564fe8e337ac162684090d2c1fc6c8
abbappii <bappi142434@gmail.com>
2022-09-03 09:20:49 +0600

    admin update

--- GIS_APP/admin.py
+++ GIS_APP/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from GIS_APP import models as gis_model
 from leaflet.admin import LeafletGeoAdmin 
 
-# Register your models here.
-
-# @admin.register(gis_model.PlotShapeModel)
-# class PlotShapeModel(admin.ModelAdmin):
-#     list_display = ('id', 'geom', 'plot')
 
 admin.site.register(gis_model.PlotShapeModel,LeafletGeoAdmin) 
 
@@ -20,25 +15,11 @@ class GasLineShapeModel(admin.ModelAdmin):
     list_display = ('id', 'dia', 'depth', 'pressure', 'len', 'geom')
 
 
-# @admin.register(gis_model.RiserShapeModel)
-# class RiserShapeModel(admin.ModelAdmin):
-#     list_display = ('id', 'dia', 'pressure', 'no_of_burn', 'load_conne', 'pressure_o', 'mon_load', 'geom')
-
-
-# @admin.register(gis_model.ValvePitShapeModel)
-# class ValvePitShapeModel(admin.ModelAdmin):
-#     list_display = ('id', 'depth', 'geom')
 admin.site.register(gis_model.ValvePitShapeModel, LeafletGeoAdmin)
 
-# @admin.register(gis_model.EndCapModel)
-# class EndCapModel(admin.ModelAdmin):
-#     list_display = ('id', 'geom')
 
 admin.site.register(gis_model.EndCapModel, LeafletGeoAdmin) 
 
-# @admin.register(gis_model.ReducerModel)
-# class ReducerModel(admin.ModelAdmin):
-#     list_display = ('id', 'geom')
 
 admin.site.register(gis_model.ReducerModel, LeafletGeoAdmin)
 
