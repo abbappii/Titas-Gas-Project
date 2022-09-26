8d4c547362f9e20c3f0a0d3b4eee0f716696f3eb
abbappii <bappi142434@gmail.com>
2022-09-26 23:41:34 +0600

    map new funciton added

--- TitasPipelineGIS/settings.py
+++ TitasPipelineGIS/settings.py
@@ -246,19 +246,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 
 #sendgrid
-
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'apikey'
-# EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
 
 # check sendinblue 
 # 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
-EMAIL_HOST = 'smtp-relay.sendinblue.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
-DEFAULT_FROM_EMAIL = 'shafiqwish@gmail.com'
+# EMAIL_HOST = 'smtp-relay.sendinblue.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = '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'
+# DEFAULT_FROM_EMAIL = 'shafiqwish@gmail.com'
 
 
 ENC_KEY = env('ENC_KEY')
